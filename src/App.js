fee21a8d96b8a3ade648e67753902eb274c9b1e0
MukulVashishth <mukulvashishth8@gmail.com>
2023-09-09 18:34:52 +0530

    styling and footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import cubes from "./assets/intro-cubes 1.png";
 import pillar from "./assets/intro-pillar 1.png";
 import appleLogo from "./assets/apple.svg";
 import androidLogo from "./assets/playStore.svg";
+import arrow from "./assets/Vector.svg";
+import frame  from "./assets/Frame.svg";
+import instaframe from "./assets/instaFrame.svg";
+import twitterframe from "./assets/TwitterFrame.svg"
+
 
 function App() {
   return (
@@ -72,19 +77,34 @@ function Body() {
         <img className="cubes" src={cubes} alt="cubes img" />
         <img className="pillar" src={pillar} alt="pillar img" />
       </div>
-      <div className="footer">
-        <div className="buttons" style={{ display: "flex" }}>
-          <button style={{ display: "flex" }}>
-            <img src={appleLogo} alt="logo-footer" />
-            <p>APP STORE</p>
-          </button>
-
-          <button style={{ display: "flex" }}>
-            <img src={androidLogo} alt="logo-footer" />
-            <p>GOOGLE PLAY</p>
-          </button>
+       <div className="footer"> 
+        <div className="buttonSection">
+          <div className="buttonSectionA my-1">
+            <div className="Appleclass my-1"><img src={appleLogo} alt="apple-logo" /></div>
+            <div className="Appleclasswritten my-1">APP STORE</div>
+          </div>
+          <div className="buttonsectionb my-1">
+          <div className="googleclass my-1"><img src={androidLogo} alt="google-logo" /></div>
+            <div className="Appleclasswritten my-1">Google Play</div>
+          </div>
         </div>
-      </div>
+        <div className="MiddleSection">
+          <img src={arrow} alt="Arrow.."/>
+        </div>
+        <div className="lastSection">
+          <div className="written my-1">Brokerage services by Cash App Investing LLC, member FINRA / SIPC. See our BrokerCheck. Investing involves risk; you may lose money. Bitcoin trading offered by Cash App. Cash App Investing does not trade bitcoin and Cash App is not a member of FINRA or SIPC. Cash App facilitates banking services through Sutton Bank and Lincoln Savings Bank, Members FDIC.</div>
+          <div className="logo1 my-1">
+           <img src={frame}/>
+          </div>
+          <div className="logo2 my-1">
+           <img src={twitterframe}/>
+          </div>
+          <div className="logo3 my-1">
+           <img src={instaframe}/>
+          </div>
+
+        </div>
+       </div>
       );
     </>
   );
